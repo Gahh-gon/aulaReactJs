1c4ab8d38edf03536c57ecc292b2c6740243e49d
Gahh-gon <77693978+Gahh-gon@users.noreply.github.com>
2022-05-24 14:20:56 -0300

    aula01-react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+<h1>Olá Mundo!</h1>
+<br/>
+<h2>Olá React!</h2>
+<br/>
+<p>Meu primeiro App</p>
+
+    </div>
+  );
+}
+
+export default App;
